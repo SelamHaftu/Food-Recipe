ed2412e51280692c19306d591640e679626a0436
Selam Haftu <selamhaftu354@gmail.com>
2020-01-26 18:30:49 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,148 +1,53 @@
 <?php
 
-$get_id = get_theme_mod( 'ocean_error_page_template' );
+get_header();
+?>
 
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
 
-$elementor  = get_post_meta( $get_id, '_elementor_edit_mode', true );
+			<section class="error-404 not-found">
+				<center><header class="page-header">
+					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'thunder' ); ?></h1>
+				</header><!-- .page-header -->
+				<div class="page-content">
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'thunder' ); ?></p>
+					</center>
 
-$get_content = oceanwp_error_page_template_content();
+					<?php
+					get_search_form();
 
-if ( 'on' == get_theme_mod( 'ocean_error_page_blank', 'off' ) ) { ?>
+					the_widget( 'WP_Widget_Recent_Posts' );
+					?>
+					<div class="widget widget_categories">
+						<h2 class="widget-title"><?php esc_html_e( 'Categories', 'thunder' ); ?></h2>
+						<ul>
+							<?php
+							wp_list_categories( array(
+								'orderby'    => 'count',
+								'order'      => 'DESC',
+								'show_count' => 1,
+								'title_li'   => '',
+								'number'     => 10,
+							) );
+							?>
+						</ul>
+					</div><!-- .widget -->
 
-	<!DOCTYPE html>
-	<html <?php language_attributes(); ?><?php oceanwp_schema_markup( 'html' ); ?>>
-		<head>
-			<meta charset="<?php bloginfo( 'charset' ); ?>">
-			<link rel="profile" href="http://gmpg.org/xfn/11">
+					<?php
+					/* translators: %1$s: smiley */
+					$thunder_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'thunder' ), convert_smilies( ':)' ) ) . '</p>';
+					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$thunder_archive_content" );
 
-			<?php wp_head(); ?>
-		</head>
+					the_widget( 'WP_Widget_Tag_Cloud' );
+					?>
 
-		<!-- Begin Body -->
-		<body <?php body_class(); ?><?php oceanwp_schema_markup( 'body' ); ?>>
+				</div><!-- .page-content -->
+				
+			</section><!-- .error-404 -->
 
-			<?php do_action( 'ocean_before_outer_wrap' ); ?>
-
-			<div id="outer-wrap" class="site clr">
-
-				<?php do_action( 'ocean_before_wrap' ); ?>
-
-				<div id="wrap" class="clr">
-
-					<?php do_action( 'ocean_before_main' ); ?>
-
-					<main id="main" class="site-main clr"<?php oceanwp_schema_markup( 'main' ); ?>>
-
-<?php
-} else {
-
-	get_header();
-
-} ?>
-
-						<?php do_action( 'ocean_before_content_wrap' ); ?>
-
-						<div id="content-wrap" class="container clr">
-
-							<?php do_action( 'ocean_before_primary' ); ?>
-
-							<div id="primary" class="content-area clr">
-
-								<?php do_action( 'ocean_before_content' ); ?>
-
-								<div id="content" class="clr site-content">
-
-									<?php do_action( 'ocean_before_content_inner' ); ?>
-
-									<article class="entry clr">
-
-										<?php
-					
-										if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'single' ) ) {
-
-											// Check if there is a template
-									        if ( ! empty( $get_id ) ) {
-
-											    // If Elementor
-											    if ( OCEANWP_ELEMENTOR_ACTIVE && $elementor ) {
-
-											        OceanWP_Elementor::get_error_page_content();
-
-											    }
-
-											    // If Beaver Builder
-											    else if ( OCEANWP_BEAVER_BUILDER_ACTIVE && ! empty( $get_id ) ) {
-
-											        echo do_shortcode( '[fl_builder_insert_layout id="' . $get_id . '"]' );
-
-											    }
-
-											    // Else
-											    else {
-
-											        // Display template content
-											        echo do_shortcode( $get_content );
-
-											    }
-
-											}
-
-										    // Else
-										    else { ?>
-
-										    	<div class="error404-content clr">
-
-													<h2 class="error-title"><?php esc_html_e( 'This page could not be found!', 'oceanwp' ) ?></h2>
-													<p class="error-text"><?php esc_html_e( 'We are sorry. But the page you are looking for is not available.', 'oceanwp' ); ?><br /><?php esc_html_e( 'Perhaps you can try a new search.', 'oceanwp' ); ?></p>
-													<?php get_search_form(); ?>
-													<a class="error-btn button" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Back To Homepage', 'oceanwp' ) ?></a>
-
-												</div><!-- .error404-content -->
-
-											<?php }
-
-										} ?>
-
-									</article><!-- .entry -->
-
-									<?php do_action( 'ocean_after_content_inner' ); ?>
-
-								</div><!-- #content -->
-
-								<?php do_action( 'ocean_after_content' ); ?>
-
-							</div><!-- #primary -->
-
-							<?php do_action( 'ocean_after_primary' ); ?>
-
-						</div><!--#content-wrap -->
-
-						<?php do_action( 'ocean_after_content_wrap' ); ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php
-// If blank page
-if ( 'on' == get_theme_mod( 'ocean_error_page_blank', 'off' ) ) { ?>
-
-			        </main><!-- #main-content -->
-
-			        <?php do_action( 'ocean_after_main' ); ?>
-			                
-			    </div><!-- #wrap -->
-
-			    <?php do_action( 'ocean_after_wrap' ); ?>
-
-			</div><!-- .outer-wrap -->
-
-			<?php do_action( 'ocean_after_outer_wrap' ); ?>
-
-			<?php wp_footer(); ?>
-
-		</body>
-	</html>
-
-<?php
-} else {
-
-	get_footer();
-
-} ?>
+get_footer();
