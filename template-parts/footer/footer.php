018a1ef21206ce2bbb4fd1ef6c9abdb76c66b0be
Selam Haftu <selamhaftu354@gmail.com>
2020-01-27 23:36:29 +0300

    modularization

---
+++ template-parts/footer/footer.php
@@ -0,0 +1,10 @@
+        
+        
+        <div class="col-lg-5 col-xs-12 about-company">
+			<div class="media">
+				<?php 
+				the_widget( 'WP_Widget_Recent_Posts');
+				?>
+			</div>
+			<p><a href="#"><i class="fa fa-facebook-square mr-1"></i></a><a href="#"><i class="fa fa-linkedin-square"></i></a></p>
+        </div>
